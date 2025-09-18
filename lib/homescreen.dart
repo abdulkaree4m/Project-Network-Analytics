d1f840ec86445b3d805206e4949dc87bebbfc703
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-18 22:49:57 +0300

    update The Home Sceen page.

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -5,7 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(body: Container(
+      
+    ),
 
     );
   }
