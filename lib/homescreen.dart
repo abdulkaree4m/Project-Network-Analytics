be15e59cdb23f03c96ff9f1a0d9279270b0fae62
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-19 00:04:30 +0300

    add back button and tittle

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../home.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -13,7 +13,38 @@ class HomeScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
         ),
-        child: SafeArea(child: Column()),
+        child: SafeArea(child: Column(
+          children: [
+            // AppBar مخصص مع زر الرجوع
+            Padding(
+              padding:  EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon:  Icon(Icons.arrow_back, color: Colors.white,size: 31),
+                    onPressed: () {
+                      // العودة إلى صفحة البداية
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) =>  Home()),
+                      );
+                    },
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    "القائمة الرئيسية",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
