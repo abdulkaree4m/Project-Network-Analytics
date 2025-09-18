cc7748335d6fd188ab07b774d11fddad718e0a01
mujeep <mujeep.com>
2025-09-18 21:12:58 +0300

    inital commit

---
+++ android/app/src/main/kotlin/com/example/network_analyzer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.network_analyzer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( NetworkApp());
+}
+class NetworkApp extends StatelessWidget {
+  const NetworkApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
