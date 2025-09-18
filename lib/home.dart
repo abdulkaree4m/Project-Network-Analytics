bdb80bfb660156b468908bbdfb1dee538df01a72
Abdulkareem <abdulkaree4m@gma1l.com>
2025-09-18 22:29:05 +0300

    Add a button, never use  .

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,53 @@ class Home extends StatelessWidget {
         "assets/imag3.jpg",
         fit: BoxFit.cover,
       ),
-    ),]
+    ),
+
+          // زر "ابدأ الاستخدام"
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SafeArea(
+              minimum:  EdgeInsets.only(bottom: 130), // المسافة من الأسفل
+              child: SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 70),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // الانتقال إلى صفحة ثانية
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>  HomeScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey.shade900, // لون الزر
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8), // زوايا دائرية بسيطة
+                        side:  BorderSide(
+                          color: Colors.greenAccent, // حدود خضراء
+                          width: 2,
+                        ),
+                      ),
+                      elevation: 8, // ظل الزر
+                      shadowColor: Colors.greenAccent,
+                    ),
+                    child:  Text(
+                      "ابدأ الاستخدام", // نص الزر
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.greenAccent,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]
     ),
 
 
